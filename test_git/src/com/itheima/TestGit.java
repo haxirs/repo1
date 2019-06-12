6cd7aa0578019e48a7dd787b021bcb167e7e258b
Kristopher <1142264663@qq.com>
2019-06-12 14:47:43 +0800

    把本地工程提交到git

---
+++ test_git/src/com/itheima/TestGit.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+}
